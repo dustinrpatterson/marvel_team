12d866403ce8110c75824ca35c69fa3c0d2e3ba6
Dustin Patterson <dustinrpatterson@gmail.com>
2016-10-03 16:11:22 -0600

    first commit

---
+++ cards-controller.js
@@ -0,0 +1,60 @@
+ 
+var dataStore = new MarvelService()
+
+
+dataStore.getCharacters(ready)
+
+function ready(data){
+  
+  update(data, '#marvel-characters')
+  
+  $('#marvel-characters').on('click', '.btn-success', function(){
+    //this function will take the player that was clicked and add them to your team through the dataStore.
+    debugger
+    dataStore.addToMyCharacters(this.id)
+    update(dataStore.getMarvelCharacters(), '#marvel-characters')
+    update(dataStore.getMyCharacters(),'#my-characters')
+  })
+  
+  $('#my-characters').on('click', '.btn-danger', function(){
+    //this will remove the character from your team
+    dataStore.removeMyCharacter(this.id)
+    update(dataStore.getMyCharacters(),'#my-characters')
+    update(dataStore.getMarvelCharacters(), '#marvel-characters')
+  })
+  
+  
+  function update(list, target){
+    //target is the id of the element where the list will be rendered
+    var elem = $(target)
+    elem.empty()
+     
+    for (var i in list) {
+      console.log(list)
+      var character = list[i];
+      console.log(character);
+      var marvelTemplate = `
+      <div class="card">
+        <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
+        <h3>${character.name}</h3>
+        <div>
+          <button class="btn-success" id="${character.id}">Add to Team</button>
+        </div>
+      <div>
+      `
+      var myTemplate = `
+      <div class="card">
+        <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
+        <h3>${character.name}</h3>
+        <div>
+          <button class="btn-danger" id="${character.id}">Remove from Team</button>
+        </div>
+      <div>
+      `
+      
+      elem.append(target == '#marvel-characters' ? marvelTemplate : myTemplate)
+    }
+    
+  }
+  
+}
---
+++ marvel-service.js
@@ -0,0 +1,61 @@
+function MarvelService(){
+  var key = '?apikey=e44062bbc76b37176b08325d5265a0f3';
+  var baseUrl = 'http://gateway.marvel.com/v1/public/'
+  
+  var dataStore = this;
+  var _marvelCharacters = [];
+  var _myCharacters = [];
+   
+  
+  dataStore.getMarvelCharacters = function getMarvelCharacters(){
+    //what should this function return
+    return _marvelCharacters
+  }
+  
+  dataStore.getMyCharacters = function getMyCharacters(){
+    //what should this function return
+    return _myCharacters
+  }
+  
+  
+  dataStore.addToMyCharacters = function addToMyCharacters(id){
+    //in order to add a character to your list you will first need to find 
+    //the character by its id in the marvelCharacters array
+    for (var i = 0; i < _marvelCharacters.length; i++) {
+        var marvelIndividual = _marvelCharacters[i];
+        if (id == marvelIndividual.id) {
+             _myCharacters.push(marvelIndividual)
+             _marvelCharacters.splice(i,1)
+        }
+    }
+  }
+  
+  dataStore.removeMyCharacter = function removeMyCharacter(id){
+    //you need to find the character that you want to remove by its id
+    //and remove it.
+    for (var i = 0; i < _myCharacters.length; i++) {
+        var myIndividual = _myCharacters[i];
+        if (id == myIndividual.id) {
+             _myCharacters.splice(i,1)
+             _marvelCharacters.push(myIndividual)
+        }
+    }
+
+  }
+  
+  
+  dataStore.getCharacters = function(callWhenDone){
+    var data = localStorage.getItem('MarvelData')
+    if(data){
+      _marvelCharacters = JSON.parse(data);
+      return callWhenDone(_marvelCharacters)
+    }
+    $.get(baseUrl + 'characters'+key, function(response){
+      localStorage.setItem('MarvelData', JSON.stringify(response.data.results))
+      _marvelCharacters = response.data.results;
+      callWhenDone(marvelCharacters)
+    })
+  }
+  
+  
+}
